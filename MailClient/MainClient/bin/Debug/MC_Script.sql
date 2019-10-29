140dffefde50583c8d8a0889b3cc5595fe8073b4
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2019-10-29 22:56:23 +0300

    Add sqlite database.

---
+++ MailClient/MainClient/bin/Debug/MC_Script.sql
@@ -0,0 +1,12 @@
+﻿-- Script Date: 29.10.2019 22:49  - ErikEJ.SqlCeScripting version 3.5.2.81
+CREATE TABLE [MailServers] (
+  [ID] INTEGER NOT NULL
+, [Name] TEXT NOT NULL
+, [IMAPAdress] TEXT NOT NULL
+, [IMAPPort] NUMERIC NOT NULL
+, [POP3Adress] TEXT NOT NULL
+, [POP3Port] INTEGER NOT NULL
+, [SMTPAdress] TEXT NOT NULL
+, [SMTPPort] INTEGER NOT NULL
+, CONSTRAINT [PK_MailServers] PRIMARY KEY ([ID])
+);
