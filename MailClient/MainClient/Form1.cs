e58d13f5edc163e07df45fdba97c9f114def9319
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2019-10-30 18:50:16 +0300

    Fix problem with DB. Add yandex, gmail, mail ports and adress.

--- MailClient/MainClient/Form1.cs
+++ MailClient/MainClient/Form1.cs
@@ -3,6 +3,9 @@ using MimeKit;
 using MailKit.Net.Smtp;
 using System.Windows.Forms;
 using System.Net;
+using System.IO;
+using System.Data.SQLite;
+using MainClient.Properties;
 
 namespace MainClient
 {
@@ -23,7 +26,25 @@ namespace MainClient
                     return;
                 }
                 else
-                    AuthorizationOnServer("smtp.yandex.ru", 465);
+                {
+                    GetConfigurationForConnection(mailTextBox.Text);
+                    try
+                    {
+                        using (var client = new SmtpClient())
+                        {
+                            client.ServerCertificateValidationCallback = (s, c, h, ex) => true;
+                            client.Connect(Settings.Default["SMTPAdress"].ToString(), Convert.ToInt32(Settings.Default["SMTPPort"]), true);
+                            client.Authenticate(mailTextBox.Text, passwordTextBox.Text);
+                            MainForm mainForm = new MainForm(mailTextBox.Text, passwordTextBox.Text);
+                            mainForm.Show();
+                            this.Hide();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
             }
             else
             {
@@ -32,6 +53,29 @@ namespace MainClient
             }
         }
 
+        private void Authorization_Load(object sender, EventArgs e)
+        {
+            if (IsInternetConnected())
+                this.Text = "Авторизация";
+            else
+                this.Text = "Авторизация  (offline mode)";
+        }
+
+        public bool IsInternetConnected()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                    this.Text = "Авторизация";
+                return true;
+            }
+            catch
+            {
+                this.Text = "Авторизация  (offline mode)";
+                return false;
+            }
+        }    
         bool IsValidEmail(string email)
         {
             try
@@ -45,37 +89,64 @@ namespace MainClient
             }
         }
 
-        private void Authorization_Load(object sender, EventArgs e)
+        public void GetConfigurationForConnection(string userMail)
         {
-            try
+            string pathToDBFileFromInternet = Path.GetFullPath(@"MailClientDB.db");
+            if (!File.Exists(pathToDBFileFromInternet))
             {
-                using (var client = new WebClient())
-                using (client.OpenRead("http://google.com/generate_204"))
-                    this.Text = "Авторизация";
-            }
-            catch
-            {
-                this.Text = "Авторизация  (offline mode)";
-            }
-        }
-
-        public void AuthorizationOnServer(string smtpAdress, int smtpPort)
-        {
-            try
-            {
-                using (var client = new SmtpClient())
+                using(var client = new WebClient())
                 {
-                    client.ServerCertificateValidationCallback = (s, c, h, ex) => true;
-                    client.Connect(smtpAdress, smtpPort, true);
-                    client.Authenticate(mailTextBox.Text, passwordTextBox.Text);
-                    MainForm mainForm = new MainForm(mailTextBox.Text, passwordTextBox.Text);
-                    mainForm.Show();
-                    this.Hide();
+                    client.DownloadFile("https://github.com/SemikAlexander/MailClient/blob/master/MailClient/MainClient/bin/MailClientDB.db", pathToDBFileFromInternet);
                 }
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.Message);
+                int index = userMail.IndexOf('@');
+                string temp = userMail.Substring(index + 1);
+                index = temp.IndexOf(".");
+                string NameServer = temp.Substring(0, index);
+                using(SQLiteConnection connection = new SQLiteConnection($"Data Source={pathToDBFileFromInternet}"))
+                {
+                    using(SQLiteCommand command = new SQLiteCommand(connection))
+                    {
+                        connection.Open();
+                        command.CommandText = $"SELECT IMAPAdress, POP3Adress, SMTPAdress, IMAPPort, POP3Port, SMTPPort FROM MailServers WHERE NameServer='{NameServer}'";
+                        command.ExecuteNonQuery();
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Settings.Default["IMAPAdress"] = (string)reader["IMAPAdress"];
+                                Settings.Default["POP3Adress"] = (string)reader["POP3Adress"];
+                                Settings.Default["SMTPAdress"] = (string)reader["SMTPAdress"];
+                                Settings.Default["IMAPPort"] = Convert.ToInt32(reader["IMAPPort"]);
+                                Settings.Default["POP3Port"] = Convert.ToInt32(reader["POP3Port"]);
+                                Settings.Default["SMTPPort"] = Convert.ToInt32(reader["SMTPPort"]);
+                            }
+                            connection.Close();
+                        }
+                    }
+                }
+                //using (SQLiteConnection connection = new SQLiteConnection(@"Data Source=MailClientDB.db"))
+                //{
+                //    connection.Open();
+                //    SQLiteCommand command = new SQLiteCommand
+                //    {
+                //        Connection = connection,
+                //        CommandText = $"SELECT IMAPAdress, POP3Adress, SMTPAdress, IMAPPort, POP3Port, SMTPPort FROM MailServers WHERE NameServer='{NameServer}'"
+                        
+                //    };
+                //    SQLiteDataReader sqlReader = command.ExecuteReader();
+                //    Settings.Default["IMAPAdress"] = (string)sqlReader["IMAPAdress"];
+                //    Settings.Default["POP3Adress"] = (string)sqlReader["POP3Adress"];
+                //    Settings.Default["SMTPAdress"] = (string)sqlReader["SMTPAdress"];
+                //    Settings.Default["IMAPPort"] = (int)sqlReader["IMAPPort"];
+                //    Settings.Default["POP3Port"] = (int)sqlReader["POP3Port"];
+                //    Settings.Default["SMTPPort"] = (int)sqlReader["SMTPPort"];
+                //    Settings.Default["POP3Checked"] = true;
+                //    Settings.Default["IMAPChecked"] = false;
+                //    connection.Close();
+                //}
             }
         }
     }
